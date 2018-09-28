aa2bd33dc3e48ab97bac61636fae1b1f12db554a
Michael Eisenbraun <eisenbm@algonquincollege.com>
2018-09-28 07:44:40 -0400

    Creates starter files for week4

---
+++ about.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Week 4 - About</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <header>
+
+  </header>
+  <main>
+    <h1>Welcome, Friend</h1>
+    
+  </main>
+  <footer>
+ 
+  </footer>
+</body>
+</html>
